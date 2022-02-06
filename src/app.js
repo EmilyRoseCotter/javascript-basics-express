1ca4ab958c889f7c33a0aa13ce182dd7a9bcf6aa
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-02-06 15:03:04 +0000

    passed strings tests execept for last one

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings');
+const {
+  sayHello,
+  firstCharacters,
+  uppercase,
+  lowercase,
+  firstCharacter,
+} = require('./lib/strings');
 
 const app = express();
 
@@ -9,6 +15,25 @@ app.get('/strings/hello/world', (req, res) => {
 
 app.get('/strings/hello/:str', (req, res) => {
   res.status(200).json({ result: sayHello(req.params.str) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  const { string } = req.params;
+  res.status(200).json({ result: firstCharacter(string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  const { string } = req.params;
+  const { query } = parseInt(req.query);
+  res.status(200).json({ result: firstCharacters(string, query) });
 });
 
 module.exports = app;
